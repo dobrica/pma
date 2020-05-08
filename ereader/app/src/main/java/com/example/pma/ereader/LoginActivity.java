fee8c3560466b8d0ccd0bbedaf2e0edc34f47cb9
dobrica <djdobrica@gmail.com>
2020-05-08 22:50:40 +0200

    login screen design touchup

--- ereader/app/src/main/java/com/example/pma/ereader/LoginActivity.java
+++ ereader/app/src/main/java/com/example/pma/ereader/LoginActivity.java
@@ -15,6 +15,8 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,6 +37,11 @@ public class LoginActivity extends AppCompatActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getSupportActionBar().hide();
+
 		setContentView(R.layout.activity_login);
 		loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory()).get(LoginViewModel.class);
 
