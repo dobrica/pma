dccdb615c71b67f7ed3f839adcaf29b557490ac0
Dejan Zlataric <dejanzlataric@outlook.com>
2020-07-01 20:05:42 +0200

    Added delete button

--- ereader/app/src/main/java/com/example/pma/ereader/ui/ListFragment.java
+++ ereader/app/src/main/java/com/example/pma/ereader/ui/ListFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,101 +22,109 @@ import com.example.pma.ereader.model.item.Item;
 import java.util.List;
 
 public abstract class ListFragment extends Fragment {
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
-    private ListFragmentViewModel viewModel;
+	/**
+	 * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+	 * device.
+	 */
+	private boolean mTwoPane;
+	private ListFragmentViewModel viewModel;
 
-    protected View buildFragment(int resId, LayoutInflater inflater, ViewGroup container, ListFragmentViewModel viewModel) {
-        View view = inflater.inflate(resId, container, false);
-        this.viewModel = viewModel;
+	protected View buildFragment(int resId, LayoutInflater inflater, ViewGroup container, ListFragmentViewModel viewModel) {
+		View view = inflater.inflate(resId, container, false);
+		this.viewModel = viewModel;
 
-        if (view.findViewById(R.id.item_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
+		if (view.findViewById(R.id.item_detail_container) != null) {
+			// The detail container view will be present only in the
+			// large-screen layouts (res/values-w900dp).
+			// If this view is present, then the
+			// activity should be in two-pane mode.
+			mTwoPane = true;
+		}
 
-        View recyclerView = view.findViewById(R.id.item_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+		View recyclerView = view.findViewById(R.id.item_list);
+		assert recyclerView != null;
+		setupRecyclerView((RecyclerView) recyclerView);
 
-        return view;
-    }
+		return view;
+	}
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new ListFragment.SimpleItemRecyclerViewAdapter( this, viewModel.getItems(), mTwoPane));
-    }
+	private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+		recyclerView.setAdapter(new ListFragment.SimpleItemRecyclerViewAdapter(this, viewModel.getItems(), mTwoPane));
+	}
 
-    public static class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+	public static class SimpleItemRecyclerViewAdapter
+		extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final FragmentManager mFragmentManager;
-        private final List<Item> mValues;
-        private final boolean mTwoPane;
-        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Item item = (Item) view.getTag();
-                if (mTwoPane) {
-                    Bundle arguments = new Bundle();
-                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.getTitle());
-                    ItemDetailFragment fragment = new ItemDetailFragment();
-                    fragment.setArguments(arguments);
-                    mFragmentManager.beginTransaction()
-                            .replace(R.id.item_detail_container, fragment)
-                            .commit();
-                } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, ItemDetailActivity.class);
-                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.getTitle());
+		private final FragmentManager mFragmentManager;
+		private final List<Item> mValues;
+		private final boolean mTwoPane;
+		private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Item item = (Item) view.getTag();
+				if (mTwoPane) {
+					Bundle arguments = new Bundle();
+					arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.getTitle());
+					ItemDetailFragment fragment = new ItemDetailFragment();
+					fragment.setArguments(arguments);
+					mFragmentManager.beginTransaction()
+						.replace(R.id.item_detail_container, fragment)
+						.commit();
+				} else {
+					Context context = view.getContext();
+					Intent intent = new Intent(context, ItemDetailActivity.class);
+					intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.getTitle());
 
-                    context.startActivity(intent);
-                }
-            }
-        };
+					context.startActivity(intent);
+				}
+			}
+		};
 
-        SimpleItemRecyclerViewAdapter(ListFragment fragment,
-                                      List<Item> items,
-                                      boolean twoPane) {
-            mValues = items;
-            mTwoPane = twoPane;
-            mFragmentManager = fragment.getParentFragmentManager();
-        }
+		SimpleItemRecyclerViewAdapter(ListFragment fragment, List<Item> items, boolean twoPane) {
+			mValues = items;
+			mTwoPane = twoPane;
+			mFragmentManager = fragment.getParentFragmentManager();
+		}
 
-        @Override
-        public ListFragment.SimpleItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_list_content, parent, false);
-            return new ListFragment.SimpleItemRecyclerViewAdapter.ViewHolder(view);
-        }
+		@Override
+		public ListFragment.SimpleItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.item_list_content, parent, false);
+			final ViewHolder viewHolder = new ViewHolder(view);
+			final View removeButton = view.findViewById(R.id.remove);
+			removeButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(final View v) {
+					mValues.remove(viewHolder.getAdapterPosition());
+					//TODO delete file from file system
+					ListFragment.SimpleItemRecyclerViewAdapter.super.notifyDataSetChanged();
+				}
+			});
+			return viewHolder;
+		}
 
-        @Override
-        public void onBindViewHolder(final ListFragment.SimpleItemRecyclerViewAdapter.ViewHolder holder, int position) {
-            holder.mContentView.setText(mValues.get(position).getTitle());
-            holder.imageView.setImageBitmap(mValues.get(position).getCoverImageBitmap());
-            holder.itemView.setTag(mValues.get(position));
-            holder.itemView.setOnClickListener(mOnClickListener);
-        }
+		@Override
+		public void onBindViewHolder(final ListFragment.SimpleItemRecyclerViewAdapter.ViewHolder holder, int position) {
+			holder.mContentView.setText(mValues.get(position).getTitle());
+			holder.imageView.setImageBitmap(mValues.get(position).getCoverImageBitmap());
+			holder.itemView.setTag(mValues.get(position));
+			holder.itemView.setOnClickListener(mOnClickListener);
+		}
 
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
+		@Override
+		public int getItemCount() {
+			return mValues.size();
+		}
 
-        class ViewHolder extends RecyclerView.ViewHolder {
-            final TextView mContentView;
-            final ImageView imageView;
+		class ViewHolder extends RecyclerView.ViewHolder {
+			final TextView mContentView;
+			final ImageView imageView;
 
-            ViewHolder(View view) {
-                super(view);
-                mContentView = view.findViewById(R.id.content);
-                imageView = view.findViewById(R.id.image);
-            }
-        }
-    }
+			ViewHolder(View view) {
+				super(view);
+				mContentView = view.findViewById(R.id.content);
+				imageView = view.findViewById(R.id.image);
+			}
+		}
+	}
 }
