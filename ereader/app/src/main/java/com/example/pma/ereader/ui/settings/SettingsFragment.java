c97e8c3887efa8faf4c338ed680fb60ef09c769a
Dejan Zlataric <dejanzlataric@outlook.com>
2020-07-03 20:26:05 +0200

    Update font from settings view

--- ereader/app/src/main/java/com/example/pma/ereader/ui/settings/SettingsFragment.java
+++ ereader/app/src/main/java/com/example/pma/ereader/ui/settings/SettingsFragment.java
@@ -17,36 +17,39 @@ import com.example.pma.ereader.ReadingActivity;
 
 public class SettingsFragment extends Fragment {
 
-    private SettingsViewModel settingsViewModel;
-    private SettingsChanged settingsChanged;
+	private SettingsViewModel settingsViewModel;
+	private SettingsChanged settingsChanged;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        SeekBar fontSeekBar = root.findViewById(R.id.fontSeekBar);
-        fontSeekBar.setProgress(12);
-        fontSeekBar.incrementProgressBy(1);
-        fontSeekBar.setMax(36);
-        this.settingsChanged = (ReadingActivity)getActivity();
-        fontSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("",String.format("Progress %d",progress));
-                settingsChanged.fontChanged(progress);
-            }
+	public View onCreateView(@NonNull LayoutInflater inflater,
+		ViewGroup container, Bundle savedInstanceState) {
+		settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
+		View root = inflater.inflate(R.layout.fragment_settings, container, false);
+		SeekBar fontSeekBar = root.findViewById(R.id.fontSeekBar);
+		fontSeekBar.setProgress(12);
+		fontSeekBar.incrementProgressBy(1);
+		fontSeekBar.setMax(36);
+		if (getActivity() instanceof ReadingActivity) {
+			this.settingsChanged = (ReadingActivity) getActivity();
+		}
+		fontSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				if (settingsChanged != null) {
+					settingsChanged.fontChanged(progress);
+				}
+			}
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
 
-            }
+			}
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
 
-            }
-        });
-        return root;
-    }
+			}
+		});
+		return root;
+	}
 
 }
