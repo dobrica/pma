c0794748bb2b4b053de366be58d69dc22a696eb2
Nikola Majstorovic <majsta93@hotmail.com>
2020-07-03 20:53:29 +0200

    brightness setting

--- ereader/app/src/main/java/com/example/pma/ereader/ui/settings/SettingsFragment.java
+++ ereader/app/src/main/java/com/example/pma/ereader/ui/settings/SettingsFragment.java
@@ -1,39 +1,73 @@
 package com.example.pma.ereader.ui.settings;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pma.ereader.MyApplication;
 import com.example.pma.ereader.R;
 import com.example.pma.ereader.ReadingActivity;
+
+import java.util.Set;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
     private SettingsChanged settingsChanged;
+    boolean success = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
         SeekBar fontSeekBar = root.findViewById(R.id.fontSeekBar);
+        SeekBar brightnessBar = root.findViewById(R.id.brightness);
         fontSeekBar.setProgress(12);
         fontSeekBar.incrementProgressBy(1);
         fontSeekBar.setMax(36);
-        this.settingsChanged = (ReadingActivity)getActivity();
+        brightnessBar.setMax(255);
+        brightnessBar.setProgress(getBrightness());
+        this.settingsChanged = (ReadingActivity) getActivity();
+
+        getPermission();
+
         fontSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("",String.format("Progress %d",progress));
+                Log.i("", String.format("Progress %d", progress));
                 settingsChanged.fontChanged(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        brightnessBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser && success) {
+                    setBrightness(progress);
+                }
             }
 
             @Override
@@ -49,4 +83,52 @@ public class SettingsFragment extends Fragment {
         return root;
     }
 
+    private void setBrightness(int brightness) {
+        if (brightness < 0) {
+            brightness = 0;
+        } else if (brightness > 255) {
+            brightness = 255;
+        }
+        ContentResolver resolver = MyApplication.getAppContext().getContentResolver();
+        Settings.System.putInt(resolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+    }
+
+    private int getBrightness() {
+        int brightness = 100;
+        try {
+            ContentResolver resolver = MyApplication.getAppContext().getContentResolver();
+            Settings.System.getInt(resolver, Settings.System.SCREEN_BRIGHTNESS);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        return brightness;
+    }
+
+    private void getPermission() {
+        boolean value;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            value = Settings.System.canWrite(MyApplication.getAppContext());
+            if (value) {
+                success = true;
+            } else {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                startActivityForResult(intent, 1000);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        boolean value;
+        if (requestCode == 1000) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                value = Settings.System.canWrite(MyApplication.getAppContext());
+                if (value) {
+                    success = true;
+                } else {
+                    Toast.makeText(getActivity(), "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
