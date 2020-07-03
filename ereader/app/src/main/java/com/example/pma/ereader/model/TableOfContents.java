508e7af42996c7559e7bc8edfac3092cf9c64b6f
dobrica <djdobrica@gmail.com>
2020-07-03 22:24:49 +0200

    border fix on content

---
+++ ereader/app/src/main/java/com/example/pma/ereader/model/TableOfContents.java
@@ -0,0 +1,10 @@
+package com.example.pma.ereader.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableOfContents {
+
+    static public List<String> items = new ArrayList<>();
+
+}
